dfac015782ba6ebb370bb7c50057eff2bfc78d5d
Mash.P <mary091192@gmail.com>
2025-10-01 12:23:39 +0200

    TestData update

--- src/test/java/helpers/TestData.java
+++ src/test/java/helpers/TestData.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-@UtilityClass
+@UtilityClass 
 public class TestData {
     
     private static final Random random = new Random();
