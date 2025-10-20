869ca95c5d5b5b60479372c50489b2e087494ec8
Mash.P <mary091192@gmail.com>
2025-10-20 23:42:02 +0200

    Добавлены Selenoid capabilities для корректной работы
    - Добавлены DesiredCapabilities с явным указанием browserName и browserVersion
    - Версия Chrome изменена на 120.0 (более распространенная на Selenoid)
    - Добавлены selenoid:options с enableVNC=true, enableVideo=false
    - Исправлено создание сессии браузера на Selenoid

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import io.qameta.allure.selenide.AllureSelenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BaseTest {
@@ -15,14 +19,22 @@ public class BaseTest {
     public static void setUp() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://revizto.com/en/");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 30000;
         
-        Configuration.remote = System.getProperty("remote", 
-            "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "120.0");
+        
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", false);
+        capabilities.setCapability("selenoid:options", selenoidOptions);
+        
+        Configuration.browserCapabilities = capabilities;
         Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
