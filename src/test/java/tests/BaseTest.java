8a673176863e70add8f6d8bb523ecefc1b754bef
Mash.P <mary091192@gmail.com>
2025-10-20 23:24:16 +0200

    Добавлен дефолтный URL Selenoid в конфигурацию
    - Configuration.remote теперь имеет дефолтное значение Selenoid URL
    - Можно переопределить через -Dremote=... для локального запуска
    - Упрощена конфигурация для Jenkins - не нужно передавать URL явно

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -15,17 +15,15 @@ public class BaseTest {
     public static void setUp() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://revizto.com/en/");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "131.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        
-        String remote = System.getProperty("remote");
-        if (remote != null) {
-            Configuration.remote = remote;
-        }
-        
-        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 30000;
+        
+        Configuration.remote = System.getProperty("remote", 
+            "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
